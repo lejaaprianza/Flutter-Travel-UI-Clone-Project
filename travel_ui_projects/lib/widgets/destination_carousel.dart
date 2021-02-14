2eb79c933e0fb0d2f8ab761d38b69240e8f764c9
lejaaprianza <lejaprianza@gmail.com>
2021-02-15 05:30:29 +0700

    insights tentang cara memposisikan gambar, boxshadow, dan border radius

--- travel_ui_projects/lib/widgets/destination_carousel.dart
+++ travel_ui_projects/lib/widgets/destination_carousel.dart
@@ -45,21 +45,67 @@ class DestinationCarousel extends StatelessWidget {
                 width: 210,
                 color: Colors.red,
                 child: Stack(
+                  alignment: Alignment.topCenter,
                   children: [
+                    Positioned(
+                      //menit 24.57
+                      bottom: 15.0,
+                      child: Container(
+                        height: 120,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${destination.activities.length} activities',
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w600,
+                                    letterSpacing: 1.2),
+                              ),
+                              Text(
+                                destination.description,
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                     Container(
-                      height: 120,
-                      width: 200,
                       decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                          borderRadius: BorderRadius.circular(20.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              offset: Offset(
+                                0.0,
+                                2.0,
+                              ),
+                              blurRadius: 6.0,
+                            )
+                          ]),
+                      child: Stack(
                         children: [
-                          Text('${destination.activities.length} activities'),
-                          Text(destination.description),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image(
+                              image: AssetImage(destination.imageUrl),
+                              height: 180,
+                              width: 180,
+                              fit: BoxFit.cover,
+                            ),
+                          )
                         ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               );
